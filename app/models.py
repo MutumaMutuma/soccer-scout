a971ea633ecb53853364045cd59675760cc54049
MutumaMutuma <lewismutuma1000@gmail.com>
2018-09-19 11:53:02 +0300

    Didi the fron e

--- app/models.py
+++ app/models.py
@@ -13,8 +13,9 @@ class User(UserMixin,db.Model):
     id = db.Column(db.Integer,primary_key = True)
     username = db.Column(db.String(255))
     email = db.Column(db.String(255),unique = True,index = True)
+    profile_pic_path = db.Column(db.String())
     pass_secure = db.Column(db.String(255))
-
+    bio = db.Column(db.String(255))
     @property
     def password(self):
             raise AttributeError('You cannot read the password attribute')
--- virtual/bin/activate.fish
+++ virtual/bin/activate.fish
@@ -29,7 +29,7 @@ end
 # unset irrelevant variables
 deactivate nondestructive
 
-set -gx VIRTUAL_ENV "/home/bashir/soccer-scout/virtual"
+set -gx VIRTUAL_ENV "/home/zamzam/Documents/soccer-scout/virtual"
 
 set -gx _OLD_VIRTUAL_PATH $PATH
 set -gx PATH "$VIRTUAL_ENV/bin" $PATH
